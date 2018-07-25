862b3c6393780d764393ce45862f91b1f2406f86
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-25 16:33:34 -0400

    More stuff

--- BaseClass/BaseClass.cs
+++ BaseClass/BaseClass.cs
@@ -11,6 +11,19 @@ namespace BaseClass
 				Console.WriteLine("LocalMachine is null!");
 			else
 				Console.WriteLine("LocalMachine is not null!");
+
+			try
+			{
+				var tryAgain = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+				if(tryAgain != null)
+					Console.WriteLine("Successfully opened registry key manually!");
+				else
+					Console.WriteLine("Failed to open registry key manually!");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
 		}
-    }
+	}
 }
